951cfa7f88824831cfad98216f93359fc59590cf
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-10-21 17:10:42 -0400

    Implemented CN mgmt and refactored global network params onto separate file

--- directory/directory.py
+++ directory/directory.py
@@ -2,15 +2,14 @@ import sys                  # for CL argument parsing
 import json                 # for encoding data sent through TCP
 import random               # for shuffling node indices during path selection
 import socket               # for TCP communication
+import configparser         # for parsing the config file
 from _thread import *       # for threaded client TCP connections
 
 ###############################################################################
 #                                                                             #
 #   TODO                                                                      #
-#    - handle invalid input hostname from TCP client                          #
-#    - if CL argument passing is accepted as feature implement flag system    #
 #    - implement encryption key mgmt                                          #
-#    - implement Circuit Number (CN) mgmt                                     #
+#    - implement TLS                                                          #
 #                                                                             #
 ###############################################################################
 
@@ -18,10 +17,40 @@ from _thread import *       # for threaded client TCP connections
 ###############################################################################
 #   GLOBALS                                                                   #
 ###############################################################################
-HOST = ''
-PORT = 5555 
-NODES = []
+HOST = None                 
+PORT = None                 
+MAX_CN = None               # maximum number of concurrent Circuit Numbers
+CNS = []                    # available Circuit Numbers
+NODES = []                  # active nodes in networks
 directory_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# parser a config file and returns a dict with the network parameters
+def parse_config():
+    parser = configparser.ConfigParser()
+    parser.read('network.conf')
+    
+    config = {}
+
+    if parser.has_section('network'):
+        items = parser.items('network')
+        for item in items:
+            config[item[0]] = item[1]
+
+    return config
+
+
+# initializes global parameters based on an input config dict
+def init_params(config):
+    global HOST, PORT, MAX_CN, CNS 
+    
+    HOST = config['host']
+    PORT = int(sys.argv[1]) if len(sys.argv) > 1 else int(config['port'])
+    MAX_CN = int(config['max_cn'])
+    CNS.extend(range(MAX_CN))
+    
+    random.shuffle(CNS)
+    
 
 # fills the NODES lsit with the machines in the LAN that are listening on PORT
 def query_network():
@@ -41,25 +70,35 @@ def query_network():
         NODES.append(hostname)
 
     random.shuffle(NODES)
+    
     print('There are {} node(s) in the network'.format(len(NODES)))
     
+
 # returns the encryption key associated with an input hostname
 def get_key(hostname):
     # TODO implement encryption key mgmt
     return "TODO_KEY_MGMT"
 
-# returns a unique circuit number
-def get_CN():
-    # TODO implement Circuit Number (CN) mgmt 
-    return 69
+
+# returns a unique CN and removes it from available CNs
+def pop_CN():
+    if CNS:
+        cn = random.choice(CNS)    
+        idx = CNS.index(cn)
+
+        return CNS.pop(idx)
+    else:
+        raise IndexError('No more available CNs')
+
 
 # makes the input CN available for future reuse
-def pop_CN(cn):
-    # TODO implement Circuit Number (CN) mgmt
-    pass
+def push_CN(cn):
+    CNS.append(cn)
+
 
 # return the string of a JSON encoded object
 # which contains the hostname, keys and CN for a random path
+# in case of invalid input or no available CNs an empty object is returned
 def get_path(dst):
     path = {}
     
@@ -69,31 +108,30 @@ def get_path(dst):
         for i in range(3):
             key = 'n{}'.format(str(i))
             path[key] = {'hostname': path_nodes[i], 'key': get_key(path_nodes[i])}
-
-        path['CN'] = get_CN()
+        
+        try:
+            path['CN'] = pop_CN()
+        except IndexError as e:
+            print(str(e))
+            path = {}
 
     return json.dumps(path)
 
+
 # thread function wich establishes TCP connection with a client
 # recieves a destination node and returns a random path
+# in case of invalid input or no available CNs an empty object is returned
 def threaded_client(conn):
-    conn.send(str.encode('\nConnected to Directory Node\n'))
-    
-    # TODO in final version no need to send prompt, no interactive comm needed
-    conn.send(str.encode('Enter hostname of destination node: '))
     data = conn.recv(1024)
         
-    if data:
-        path = get_path(data.decode('utf-8').rstrip())
-        if path:
-            conn.sendall(str.encode(path))
-        else:
-            # TODO handle invalid input hostname from TCP client
-            conn.sendall(str.encode('Illegal Input'))
+    path = get_path(data.decode('utf-8').rstrip())
+
+    conn.sendall(str.encode(path))
 
     conn.close()
 
-# configures threaded TCP server on PORT
+
+# creates worker threads for the TCP server
 def start_server():
     directory_socket.bind((HOST,PORT))
     directory_socket.listen(len(NODES))
@@ -102,28 +140,29 @@ def start_server():
 
     while True:
         conn, addr = directory_socket.accept()
-        print('Connected to: {}:{}'.format(addr[0],str(addr[1])))
+        print('Connected to: {}:{}'.format(addr[0], str(addr[1])))
         start_new_thread(threaded_client,(conn,))
 
-# closes the listening TCP server on PORT
+
 def stop_server():
     directory_socket.close()
     print('Directiry Node DOWN')
 
-# main entry point
+
 def main():
-    # TODO if CL argument passing is accepted as feature implement flag system
-    if len(sys.argv) > 1:
-        global PORT 
-        PORT = int(sys.argv[1])
+    config = parse_config()
+    if config:
+        init_params(config)
+    else:
+        sys.exit("Unable to open network.conf")
 
     query_network()
     try:
         start_server()
     except (socket.error, KeyboardInterrupt) as e:
-        print(str(e))
         stop_server()
-        sys.exit(0)
+        sys.exit(str(e))
+
 
 if __name__ == '__main__':
     main()
