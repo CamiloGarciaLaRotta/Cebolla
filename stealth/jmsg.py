20f409d99997e5be4b8da7b8f68890d8f733f037
Harley WILTZER <hwiltz@ds2.CS.McGill.CA>
2017-10-25 11:47:58 -0400

    Refactored JSONMessage slightly

--- stealth/jmsg.py
+++ stealth/jmsg.py
@@ -7,10 +7,13 @@ class MessageType(Enum):
 
 class JSONMessage(object):
     def __init__(self, message_type, addr, msg):
-        self.s = '{"type": "' + str(message_type) + '", "addr": "' + str(addr) + '", "data": "' + msg + '"}'
+        self.d = {}
+        self.d["data"] = msg
+        self.d["addr"] = addr
+        self.d["type"] = str(message_type)
     
     def to_string(self):
-        return self.s
+        return json.dumps(self.d)
 
     def to_dict(self):
-        return json.loads(self.s)
+        return self.d
