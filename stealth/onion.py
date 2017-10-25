4695d9045c83def13952a4bd8d8b53c3874d50e2
Harley WILTZER <hwiltz@ds2.CS.McGill.CA>
2017-10-25 12:40:39 -0400

    #4 - adds new random initialization vector for each message

--- stealth/onion.py
+++ stealth/onion.py
@@ -1,9 +1,11 @@
 import stealth
 import jmsg
 import json
+import random
 from stealth import AESProdigy
 from jmsg import JSONMessage
 from jmsg import MessageType
+from Crypto.Random.Fortuna.FortunaGenerator import AESGenerator
 
 PATH_LENGTH = 3
 
@@ -12,24 +14,37 @@ onions = []
 class Originator(object):
     def __init__(self):
         self.path = dummy_get_onion_circuit()
+        self.gens = [AESGenerator() for i in range(len(self.path))]
+        for c in range(len(self.path)):
+            self.gens[c].reseed(self.path[c][1])
 
     # Creates the full encryption-layered message
     def create_onion(self, msg, dst):
         msg = JSONMessage(MessageType.Data, dst, msg) # Mark the destination message as MessageType.Data for now, just for testing
         for i in range(len(self.path)-1,-1,-1): # Cycles list in reverse order
-            msg = add_layer(msg.to_string(), self.path[i][1], self.path[i][0])
+            msg = self.add_layer(msg.to_string(), i)
         return msg
+
+    # Adds another onion layer to a message
+    def add_layer(self, msg, node):
+        machine = AESProdigy(self.path[node][1], self.gens[node].pseudo_random_data(16))
+        msg = machine.encrypt(msg)
+        return JSONMessage(MessageType.Onion, self.path[node][0], msg)
 
 class OnionNode(object):
     def __init__(self):
         self.key =  ''
+        self.rng = AESGenerator()
     
     def set_key(self, key):
         self.key = key
 
+    def set_seed(self, seed):
+        self.rng.reseed(seed)
+
     # Decrypts an onion layer to retrieve the underlying JSON for the next layer
     def peel_layer(self, cipher):
-        machine = AESProdigy(self.key)
+        machine = AESProdigy(self.key, self.rng.pseudo_random_data(16))
         data = json.loads(machine.decrypt(cipher))
         return data
 
@@ -41,11 +56,7 @@ def dummy_get_onion_circuit():
         path.append((i, k))
         onion = OnionNode()
         onion.set_key(k)
+        onion.set_seed(k)
         onions.append(onion)
     return path
 
-# Adds another onion layer to a message
-def add_layer(msg, key, dst):
-    machine = AESProdigy(key)
-    msg = machine.encrypt(msg)
-    return JSONMessage(MessageType.Onion, dst, msg)
--- stealth/stealth.py
+++ stealth/stealth.py
@@ -11,25 +11,26 @@ _pad = lambda msg: msg + (BS - len(msg) % BS) * chr(BS - len(msg) % BS).encode()
 # removes padding from _pad
 _trunc = lambda msg: msg[:-ord(msg[len(msg)-1:])]
 
-iv = chr(0) * BS #Initialization vector for AES
+#iv = chr(0) * BS #Initialization vector for AES
 rand_stream = Random.new() #New stream of random numbers
 
 def get_random_key(nbytes):
     return rand_stream.read(nbytes)
 
 class AESProdigy(object):
-    def __init__(self, key):
+    def __init__(self, key, iv):
         self.key = key
+        self.iv = iv
 
     def encrypt(self, message):
         message = message.encode()
         pm = _pad(message)
-        machine = AES.new(self.key, AES.MODE_CBC, iv)
+        machine = AES.new(self.key, AES.MODE_CBC, self.iv)
         cipher = machine.encrypt(pm)
         return base64.b64encode(cipher).decode('utf-8')
 
     def decrypt(self, cipher):
         cipher = base64.b64decode(cipher)
-        machine = AES.new(self.key, AES.MODE_CBC, iv)
+        machine = AES.new(self.key, AES.MODE_CBC, self.iv)
         pm = machine.decrypt(cipher)
         return _trunc(pm).decode('utf-8')
