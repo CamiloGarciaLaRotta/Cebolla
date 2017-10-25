f2394281051d6543b8fdda4365be57bd2f10fc82
Harley WILTZER <hwiltz@ds2.CS.McGill.CA>
2017-10-25 12:50:52 -0400

    Adds forwardonion.py, to interactively set message and destination and observe forward onion process

---
+++ stealth/forwardonion.py
@@ -0,0 +1,34 @@
+import onion
+import stealth
+from onion import Originator
+from onion import OnionNode
+
+# TEST CASE FOR THE ONION LAYERING
+# Onion nodes kept in an array in onion.py
+# IP addresses/domain names are replaced by the index of the onion node
+# For the sake of the test, the message intended for the destination is typed as MessageType.Data, to give the while loop an edge condition
+
+originator = Originator()
+while True:
+    msg = raw_input("Enter message: ")
+    address = raw_input("Enter destination: ")
+    packet = originator.create_onion(msg, address)
+    print('Originator creates:')
+    print(packet.to_dict())
+    print('')
+
+    d = packet.to_dict()
+    while(True):
+        if d["type"] == "MessageType.Data":
+            print('========================')
+            print('To: ' + d["addr"])
+            print(d["data"])
+            print('========================')
+            break
+        addr = int(d["addr"])
+        packet = d["data"]
+        packet = onion.onions[addr].peel_layer(packet)
+        print('Node ' + str(addr) + ' creates:')
+        print(packet)
+        print('')
+        d = packet
