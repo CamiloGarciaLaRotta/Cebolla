1325fd8f9283aa9c8a10133bd92fb2a439b45c2b
stumash <stumash1@gmail.com>
2017-10-29 01:52:28 -0400

    small default timeout on tcp when querying network

--- directory/directory.py
+++ directory/directory.py
@@ -4,13 +4,15 @@ import random               # for shuffling node indices during path selection
 import socket               # for TCP communication
 from _thread import *       # for threaded client TCP connections
 
-###############################################################################
+
+
 #   TODO
 #    - incorporate encryption key mgmt
 
 
-###############################################################################
+
 #   CLI ARGS
+###############################################################################
 
 parser = argparse.ArgumentParser() # instantiate cli args parser
 
@@ -27,32 +29,33 @@ if args.port < 5551 or args.port > 5557: # 7 group members, each get a port
     parser.error("port must satisfy: 5551 <= port < 5557")
 
 
-################################################################################
+
 #   GLOBALS
+#################################################################################
 
-HOST = ""                              # empty string => all IPs of this machine
-PORT = args.port                         # port for server to listen on, cli arg
+HOST = ""                            # empty string => all IPs of this machine
+PORT = args.port                     # port for server to listen on, cli arg
 
-MAX_NODES = args.max_nodes                # max number of nodes in onion network
-NODE_FORMAT = "lab2-{}.cs.mcgill.ca" # python format string of nodes' domain names
-NODES = [NODE_FORMAT.format(i) for i in range(1, MAX_NODES+1)]   # all nodenames
+MAX_NODES = args.max_nodes           # max number of nodes in onion network
+NODE_FORMAT = "lab2-{}.cs.mcgill.ca" # python formatstring of nodes' domain names
+NODES = [NODE_FORMAT.format(i) for i in range(1, MAX_NODES+1)]
 
-# socket listens on HOST:PORT
 LISTEN_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LISTEN_SOCKET.bind((HOST, PORT))
 LISTEN_SOCKET.listen(MAX_NODES)
 
 
-###############################################################################
+
 #   MAIN
+#################################################################################
 
 def main():
     global NODES
 
     NODES = list( filter(ping_node, NODES) ) # keep nodes that respond to ping
+    NODES = list( zip(NODES, map(get_node_pubkey, NODES)) )  # and get pubkeys
     print(NODES)
     exit()
-    NODES = list( zip(NODES, map(get_node_pubkey, NODES)) )   # and get pubkeys
 
     # try:
         # run_directory_node()
@@ -61,22 +64,30 @@ def main():
         # sys.exit(str(e))
 
 
-###############################################################################
+
 #   INITIALIZE NODES LIST, GET PUBKEYS
+###############################################################################
 
 # return true if node is activated
 def ping_node(ndn): # ndn = node domain name
+    socket.setdefaulttimeout(0.7) # to ping node faster
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     exit_code = s.connect_ex((ndn,PORT))
     s.close()
+
+    socket.setdefaulttimeout(None) # restore default
     return not exit_code
 
 # get node pubkey
 def get_node_pubkey(ndn): # ndn = node domain name
-    return "<TODO: put pubkey here>"
+    # TODO fill this in
+    return "not a real pubkey"
 
-###############################################################################
+
+
 #   THE DIRECTORY NODE SERVER
+###############################################################################
 
 def run_directory_node():
     print('Directory Node UP')
@@ -105,8 +116,10 @@ def threaded_client(conn):
 def get_path(dst):
     return random.sample(NODES, 3)
 
-###############################################################################
+
+
 #   RUN MAIN
+###############################################################################
 
 if __name__ == '__main__':
     main()
