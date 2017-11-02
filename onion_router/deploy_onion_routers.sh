d9333bdeda3089d44dc82d2cb0795e2fa72f361f
stumash <stumash1@gmail.com>
2017-11-02 01:57:22 -0400

    clean onion_router.py, add dummy_dest deploy scripts, fix deploy scripts documentation

--- onion_router/deploy_onion_routers.sh
+++ onion_router/deploy_onion_routers.sh
@@ -12,16 +12,16 @@ cd "$THIS_DIR"
 read -r -d '' helpstring <<DOC
 deploy_onion_routers.sh usage:
 
-deploy_onion_routers.sh -u <user> -d <dirCebolla> [-b <branch>] -p <port>
- @param  user         the username to be used to login to the remote host
+deploy_onion_routers.sh -m maxNodes -u <user> -d <dirCebolla> [-b <branch>] -p <port>
  @param  maxNodes     the number of onion router nodes to set up. 1 <= maxNodes <= 50
+ @param  user         the username to be used to login to the remote host
  @param  dirCebolla   the path to the dirCebolla directory on the remote host
  @option branch       if given, the github branch version to use, else use local version
  @param  port         the port number for the onion router node to listen on. 5551 <= port <= 5557
 
 What it does:
 for i in [1 ... maxNodes]
- 1. login to a mcgill machine (specifically lab2-i.cs.mcgill.ca)
+ 1. login to lab2-i.cs.mcgill.ca
  2. cd to Cebolla directory
  3. if branchname arg given, checkout and pull branch. else, scp local copy
  4. run onion_router.py on the specified port to start up the onion_router node server
@@ -81,7 +81,7 @@ fi
 
 
 
-#     UPDATE AND RUN DIRECTORY.PY ON THE REMOTE
+#     UPDATE AND RUN onion_router.py ON THE REMOTE
 ####################################################
 
 trap 'continue' SIGINT # ^C goes to next iteration of loop below
--- onion_router/onion_router.py
+++ onion_router/onion_router.py
@@ -3,6 +3,8 @@ import json
 import socket
 import argparse
 
+
+
 #       CLI ARGS
 #########################################################
 
@@ -19,76 +21,103 @@ if args.port < 5551 or args.port > 5557: # 7 group members, each get a port
 
 
 
-#       RUN DUMMY SERVER
+#       GLOBALS
 ########################################################
 
 PORT = args.port
 HOST = ""
-SOCKET_LISTEN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SOCKET_LISTEN.bind((HOST,PORT))
-SOCKET_LISTEN.listen(1)
+LISTENER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+LISTENER_SOCKET.bind((HOST,PORT))
+LISTENER_SOCKET.listen(50)
+
+
+
+#       MAIN
+########################################################
 
 def main():
     try:
         while 1:
-            conn_socket, client_addr = SOCKET_LISTEN.accept()
+            # accept connection from previous ('backward') node in path
+            back_conn, client_addr = LISTENER_SOCKET.accept()
             print('Connected to: {}:{}'.format(client_addr[0], str(client_addr[1])))
 
-            # new thread for backward data passing
-            t = threading.Thread(target=two_way_setup, args=(conn_socket,))
+            # new thread for self-setup as onion_router
+            t = threading.Thread(target=two_way_setup, args=(back_conn,))
             t.start()
 
     except (socket.error, KeyboardInterrupt):
-        SOCKET_LISTEN.close()
+        LISTENER_SOCKET.close()
 
+
+
+#       SETUP TWO TCP CONNECTIONS
+########################################################
+
+# need to establish symkey with back_conn, connect to forw_conn
 def two_way_setup(back_conn):
     msg = back_conn.recv(2048).decode('utf-8').rstrip()
 
-    # (TODO:decrypt data, initialize symkey, send ACK)
+    # (TODO:decrypt data, initialize+respond with symkey. for now send 'ACK')
     back_conn.send("ACK".encode('utf-8'))
 
-    # Wait for first-ever data onion
+    # Wait for first-ever onion from back_conn
     print('[Onion] Waiting for data onion...')
-    msg = back_conn.recv(2048).decode('utf-8')
+    msg = back_conn.recv(2048).decode('utf-8').rstrip()
     print('[Onion] Got data onion.')
+
     # (TODO:decrypt)
+
+    # parse onion to find out who to send to and what to send
     msg_dict = json.loads(msg)
     msg_data = msg_dict["data"]
     msg_addr = msg_dict["addr"]
 
+    # connect to forw_conn and pass along data from back_conn
     forw_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('[Onion] Connecting to next onion node...')
     forw_conn.connect((msg_addr, PORT))
     print('[Onion] Connected.')
     forw_conn.send(msg_data.encode('utf-8'))
-        	
+
+    # now that two way communication is established, pass data back and forth forever
+
     t = threading.Thread(target=backward_transfer, args=(forw_conn, back_conn))
     t.start()
     forward_transfer(back_conn, forw_conn)
 
-def backward_transfer(forw_conn, back_conn):
-    while True:
-        msg = forw_conn.recv(2048).decode('utf-8').rstrip()
 
-        # (TODO: encrypt)
-        # 'msg = decrypt(msg)'
 
-        # send it on down the line!
-        back_conn.send(msg.encode('utf-8'))
+#       PASS DATA FORWARD AND BACKWARD FOREVER
+########################################################
 
 def forward_transfer(back_conn, forw_conn):
     while True:
         msg = back_conn.recv(2048).decode('utf-8').rstrip()
 
-        # (TODO:decrypt)
+        # (TODO: decrypt)
 
         # json parse
         msg_dict = json.loads(msg)
         msg_data = msg_dict["data"]
         msg_addr = msg_dict["addr"]
 
-        # send it on down the line!
+        # pass it on
         forw_conn.send(msg_data.encode('utf-8'))
+
+def backward_transfer(forw_conn, back_conn):
+    while True:
+        msg = forw_conn.recv(2048).decode('utf-8').rstrip()
+
+        # (TODO: encrypt)
+
+        # pass it on
+        back_conn.send(msg.encode('utf-8'))
+
+
+
+#       RUN MAIN
+########################################################
 
 if __name__ == "__main__":
     main()
