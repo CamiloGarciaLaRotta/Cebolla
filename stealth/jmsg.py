23ee58a60004b1d10893e76ef77797fcee1c46a0
Harley Wiltzer <harleyw@hotmail.com>
2017-11-03 22:48:56 -0400

    Added port options to JSONMessage, fought with encodings for python3 with JSON/RSA encryption again

--- stealth/jmsg.py
+++ stealth/jmsg.py
@@ -1,11 +1,12 @@
 import json
 
 class JSONMessage(object):
-    def __init__(self, message_type, addr, msg):
+    def __init__(self, message_type, addr, msg, port=80):
         self.d = {}
         self.d["data"] = msg
         self.d["addr"] = addr
         self.d["type"] = str(message_type)
+        self.d["port"] = port
     
     def to_string(self):
         return json.dumps(self.d)
