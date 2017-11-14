76c5e579cdd248db1f268920b85a6928fa3b61df
Spiros Mavroidakos <Dakos-Industries@users.noreply.github.com>
2017-11-14 11:09:27 -0500

    Fixed bug with dummy desyination set up

--- networkInitializer.bash
+++ networkInitializer.bash
@@ -3,7 +3,7 @@
 #input 
 #	$1: The number of path nodes: will be propted to sign in
 #	$2: The users cs account name
-
+#       $3: The cs path for the destination 
 x=0
 while [ $x -lt $1 ]
 	do
@@ -23,6 +23,6 @@ while [ $x -le $1 ]
   done
       
 tmux send-keys -t directoryNode "ssh $2@cs-1.cs.mcgill.ca" Enter;\
-tmux send-keys -t dummyDest "ssh $2@cs-6.cs.mcgill.ca" Enter;\
+tmux send-keys -t dummyDest "ssh $2@$3" Enter;\
 tmux send-keys -t client "ssh $2@cs-3.cs.mcgill.ca" Enter ;\
 	
