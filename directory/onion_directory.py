293fdfabf7864f63486c936db46cb6d720f7a41b
Stuart Mashaal <stumash1@gmail.com>
2017-11-16 19:22:20 -0500

    change to onion_directory.py

--- directory/onion_directory.py
+++ directory/onion_directory.py
@@ -15,11 +15,11 @@ from stealth import AESProdigy
 ###############################################################################
 
 # define cli positional args
-parser = argparse.ArgumentParser() 
+parser = argparse.ArgumentParser()
 
 parser.add_argument("max_nodes", help="Quantity of nodes in network", type=int)
 parser.add_argument("port", help="Port to listen on for path requests", type=int)
-parser.add_argument("dir_port", 
+parser.add_argument("dir_port",
                     help="Port that routers will listen for Directory", type=int)
 parser.add_argument("-v", "--verbose",
                     help="level of logging verbose", action="store_true")
@@ -29,25 +29,22 @@ args = parser.parse_args() # parse the args
 # validate args against conditions
 if args.max_nodes > 50 or args.max_nodes < 1: # no more than 50 nodes
     parser.error("max_nodes must satisfy: 1 <= max_nodes <= 50")
-# TODO disabled to a facilitate testing
-#if args.port < 5551 or args.port > 5557: # 7 group members, each get a port
-#    parser.error("port must satisfy: 5551 <= port <= 5557")
 
 
 #   GLOBALS
 #################################################################################
 
+KEYPAIR = stealth.RSAVirtuoso()
 
-KEYPAIR = stealth.RSAVirtuoso()             # directory's encryption key pairs
+# max number of routers in onion network
+MAX_NODES = args.max_nodes
 
-ROUTERS = []                                # available routers in the onion network
+# list of onion routers
+NODES = ['lab2-{}'.format(i) for i in range(1, MAX_NODES+1)]
 
-HOST = ""                            
-PORT = args.port                     
-MAX_NODES = args.max_nodes                  # max number of nodes in onion network
-
+# a socket for path requests from originators
 LISTEN_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-LISTEN_SOCKET.bind((HOST, PORT))
+LISTEN_SOCKET.bind(("", args.port))
 LISTEN_SOCKET.listen(MAX_NODES)
 
 
@@ -55,22 +52,21 @@ LISTEN_SOCKET.listen(MAX_NODES)
 #################################################################################
 
 def main():
-    global ROUTERS
-    
-    if args.verbose: print('[Status] Querying network nodes...')
-    
-    # build list of available routers with their respective encryption keys
-    nodes = ['lab2-{}'.format(i) for i in range(1, MAX_NODES+1)]
-    up_nodes = list( filter(ping_node, nodes) ) 
-    nodes_keys = zip(up_nodes, map(get_node_pubkey, up_nodes))
-    ROUTERS = [{'addr':x[0], 'key':x[1], 'port': PORT} for x in nodes_keys]
+    global NODES
 
-    if len(ROUTERS) < 3:
+    if args.verbose: print('[Status] Querying network NODES...')
+
+    NODES     = filter(ping_node, NODES)
+    node_keys = map(get_node_pubkey, NODES)
+    NODES = [{'addr':a, 'key':k, 'port':args.port} for a,k in zip(NODES, node_keys)]
+
+    if len(NODES) < 3:
         print('[Status] Not enough onion nodes running in network')
         print('[Error] Directory DOWN')
         sys.exit(1)
 
     if args.verbose: print('[Status] Directory UP')
+
     try:
         run_directory_node()
     except (socket.error, KeyboardInterrupt) as e:
@@ -78,33 +74,33 @@ def main():
         if args.verbose: print('[Error] Directory DOWN')
 
 
-
 #   INITIALIZE NODES LIST, GET PUBKEYS
 ###############################################################################
 
 # return true if node is listening on dir port
-def ping_node(ndn): # ndn = node domain name
-    
+def ping_node(node):
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    exit_code = s.connect_ex((ndn,args.dir_port))
+    exit_code = s.connect_ex((node,args.dir_port))
     s.close()
 
-    if args.verbose: 
+    if args.verbose:
         node_status = "LISTENING" if not exit_code else "DOWN"
-        print("Node {} is {}".format(ndn,node_status))
+        print("Node {} is {}".format(node,node_status))
 
     return not exit_code
 
-def get_node_pubkey(ndn): # ndn = node domain name
+# return node's pubkey as string
+def get_node_pubkey(node):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ndn,args.dir_port))
+    s.connect((node,args.dir_port))
 
     s.sendall(b'get_key')
     key = s.recv(2048).decode('utf-8')
 
     s.close()
-    
-    return key 
+
+    return key
 
 
 #   THE DIRECTORY NODE SERVER
@@ -129,11 +125,11 @@ def shut_down_directory_node():
 
 def handle_path_request(conn):
     data = conn.recv(1024).decode('utf-8').rstrip()
-    if args.verbose: print('[Status] Recieved path request from {}'.format(data)) 
+    if args.verbose: print('[Status] Recieved path request from {}'.format(data))
 
     pubkey = KEYPAIR.get_public_key().exportKey()
     conn.sendall(pubkey)
-    
+
     data = conn.recv(2048).decode('utf-8').rstrip()
 
     condata = KEYPAIR.extract_path_data(data)
@@ -146,7 +142,7 @@ def handle_path_request(conn):
     rng.reseed(condata[0])
     aesmachine = AESProdigy(condata[0], rng.pseudo_random_data(16))
     ciphertext = aesmachine.encrypt(json.dumps(path))
-    
+
     conn.sendall(ciphertext.encode('utf-8'))
 
     conn.close()
