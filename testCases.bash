164e0e66bcf31099c7a7477e4dfce977cfc04d12
Spiros <spirosmavroidakos@gmail.com>
2017-11-20 13:19:01 -0500

    Added automated tests

---
+++ testCases.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#A bunch of test cases for the onion network
+
+tmux send-keys -t client "Tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+tmux send-keys -t client "Tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester Tester Tester tester" Enter ;\
+sleep 0.5
+
+
+
